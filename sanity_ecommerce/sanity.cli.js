b909547472caef28a5bc5371f0159eda9115a692
talhachaudharyy <141918104+talhachaudharyy@users.noreply.github.com>
2023-09-05 16:22:30 +0500

    my first commit website

---
+++ sanity_ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '8xl83ors',
+    dataset: 'production'
+  }
+})
---
+++ sanity_ecommerce/schemas/index.js
@@ -0,0 +1,6 @@
+export const schemaTypes = [product, banner]
+
+
+import product from './product';
+import banner from './banner';
+
